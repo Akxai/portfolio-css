6efec6abfa3f57f712a22764c8ee6157e1ea5fa6
Akshay Aileni <akshayakshay5543@gmail.com>
2023-05-13 22:04:31 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,196 @@
+const body = document.querySelector("body");
+const sliderInput = document.querySelector("#slider-input");
+const allBoxes = document.querySelectorAll(".box");
+const allPills = document.querySelectorAll(".pill");
+const allHiddenPills = document.querySelectorAll(".hidden-pill");
+const allArrows = document.querySelectorAll(".arrow");
+const expandPill = document.querySelector("#expand");
+const xLetterPath = document.querySelector("#x-letter");
+const xLetterSVG = document.querySelector("#x-letter-svg");
+const xBox = document.querySelector(".x-box");
+const socialFan = document.querySelector(".social-fan");
+const reversePill = document.querySelector("#reverse");
+const boxContainer = document.querySelector(".box-container");
+const iconPath = document.querySelector("#icon");
+const hiddenBox = document.querySelector(".hidden-box");
+const textBox = document.querySelector(".hidden-box .text-box");
+
+let paletteIndex = 0;
+const socialFanIndex = 1;
+const xLetterIndex = 11;
+const rotateIconIndex = 3;
+
+xLetterSVG.style.fill = colorPalettes[paletteIndex][xLetterIndex].fill;
+allPills.forEach(
+  (pill, i) =>
+    (pill.style.backgroundColor = colorPalettes[paletteIndex][i].fill)
+);
+
+allHiddenPills.forEach(
+  (hiddenPill) =>
+    (hiddenPill.style.backgroundColor =
+      colorPalettes[paletteIndex][socialFanIndex].fill)
+);
+
+const expand = () => {
+  if (hiddenBox.clientWidth !== 0) {
+    textBox.classList.add("hidden");
+    setTimeout(() => (hiddenBox.style.width = "0px"), 200);
+    // hiddenBox.style.width = "0px";
+  } else {
+    hiddenBox.style.width = "1700px";
+    setTimeout(() => textBox.classList.remove("hidden"), 500);
+  }
+};
+
+expandPill.addEventListener("click", expand);
+
+const reverse = () => {
+  if (boxContainer.style.flexWrap === "wrap") {
+    boxContainer.style.flexWrap = "wrap-reverse";
+  } else {
+    boxContainer.style.flexWrap = "wrap";
+  }
+};
+
+reversePill.addEventListener("click", reverse);
+
+const addTheme = (
+  bodyBackgroundColor,
+  strokeWidth,
+  svgFill,
+  opacity,
+  lineColor,
+  borderRadius,
+  boxBackgroundColor,
+  pillBackgroundColor
+) => {
+  body.style.backgroundColor = bodyBackgroundColor;
+  xLetterPath.style.strokeWidth = strokeWidth;
+  xLetterSVG.style.fill =
+    svgFill || colorPalettes[paletteIndex][xLetterIndex].fill;
+  xLetterSVG.style.opacity = opacity;
+  xBox.style.backgroundColor =
+    boxBackgroundColor || colorPalettes[paletteIndex][xLetterIndex].fill;
+  iconPath.style.stroke =
+    lineColor || colorPalettes[paletteIndex][rotateIconIndex].altStroke;
+  iconPath.style.strokeWidth = strokeWidth;
+
+  allBoxes.forEach(
+    (box, i) =>
+      (box.style.backgroundColor =
+        boxBackgroundColor || colorPalettes[paletteIndex][i].fill)
+  );
+
+  allPills.forEach((pill, i) => {
+    pill.style.opacity = opacity;
+    pill.style.backgroundColor =
+      pillBackgroundColor || colorPalettes[paletteIndex][i].fill;
+    pill.style.borderWidth = strokeWidth;
+    pill.style.borderColor =
+      lineColor || colorPalettes[paletteIndex][i].altStroke;
+    pill.style.borderBlockStyle = "solid";
+    pill.style.borderRadius = borderRadius;
+  });
+
+  allHiddenPills.forEach((hiddenPill) => {
+    hiddenPill.style.opacity = opacity;
+    hiddenPill.style.borderWidth = strokeWidth;
+    hiddenPill.style.backgroundColor =
+      pillBackgroundColor || colorPalettes[paletteIndex][socialFanIndex].fill;
+    hiddenPill.style.borderColor =
+      lineColor || colorPalettes[paletteIndex][socialFanIndex].altStroke;
+    hiddenPill.style.borderRadius = borderRadius;
+  });
+
+  allArrows.forEach((arrow) => {
+    arrow.style.borderBlockStyle = "solid";
+    arrow.style.borderColor = lineColor;
+    arrow.style.borderWidth = "0 " + strokeWidth + " " + strokeWidth + " 0";
+    arrow.style.opacity = opacity;
+  });
+};
+
+const moveSlider = () => {
+  const sliderInput = document.querySelector("#slider-input");
+  const sliderValue = sliderInput.value;
+  if (sliderValue == 0) {
+    addTheme(
+      "white",
+      "12px",
+      null,
+      1,
+      "rgb(38, 38, 38)",
+      "100px",
+      "white",
+      null
+    );
+  }
+  if (sliderValue > 1 && sliderValue <= 3) {
+    addTheme("white", "2px", "white", 0.5, null, "75px", null, "white");
+  }
+  if (sliderValue >= 4 && sliderValue <= 6) {
+    addTheme("white", "1px", "white", 0.5, null, "90px", null);
+  }
+  if (sliderValue >= 7 && sliderValue < 9) {
+    addTheme(
+      "white",
+      "2px",
+      "white",
+      0.5,
+      "rgb(38,38,38)",
+      "50px",
+      null,
+      "white"
+    );
+  }
+  if (sliderValue == 10) {
+    addTheme(
+      "rgb(38,38,38)",
+      "12px",
+      "white",
+      1,
+      "black",
+      0,
+      "transparent",
+      "white"
+    );
+  }
+};
+
+sliderInput.addEventListener("input", moveSlider);
+
+const changePalette = () => {
+  xLetterPath.classList.add("pulse");
+  if (paletteIndex >= 2) {
+    paletteIndex = 0;
+  } else {
+    paletteIndex++;
+  }
+  moveSlider();
+  setTimeout(() => xLetterPath.classList.remove("pulse"), 500);
+};
+
+xBox.addEventListener("click", changePalette);
+
+const profils = document.querySelectorAll("#profil");
+console.log(profils[0].attributes.colors.nodeValue);
+console.log(profils[1].attributes.colors.nodeValue);
+console.log(profils[2].attributes.colors.nodeValue);
+console.log(profils[3].attributes.colors.nodeValue);
+
+sliderInput.addEventListener("input", function () {
+  const sliderValue = sliderInput.value;
+  if (sliderValue == 10) {
+    profils.forEach((profil) => {
+      profil.attributes.colors.nodeValue =
+        "outline:#121331,primary:black,secondary:black";
+    });
+  } else {
+    profils.forEach((profil) => {
+      // Assign the original colors to the attributes
+      profil.attributes.colors.nodeValue =
+        "outline:#121331,primary:#121331,secondary:#ffc738,tertiary:#4bb3fd,quaternary:#ffc738,quinary:#f9c9c0";
+    });
+  }
+});
---
+++ colors.js
@@ -0,0 +1,105 @@
+const colorPalettes = [
+  [
+    {
+      altStroke: "#1a2f38",
+      fill: "#264653",
+    },
+    {
+      altStroke: "#1b4d4c",
+      fill: "#287271",
+    },
+    {
+      altStroke: "#1b665d",
+      fill: "#2a9d8f",
+    },
+    {
+      altStroke: "#6b8a60",
+      fill: "#8ab17d",
+    },
+    {
+      altStroke: "#898a54",
+      fill: "#babb74",
+    },
+    {
+      altStroke: "#ba9d56",
+      fill: "#e9c46a",
+    },
+    {
+      altStroke: "#c2804c",
+      fill: "#f4a261",
+    },
+    {
+      altStroke: "#b5573f",
+      fill: "#e76f51",
+    },
+    {
+      altStroke: "#9e789e",
+      fill: "#ba8fba",
+    },
+    {
+      altStroke: "#42466e",
+      fill: "#595f94",
+    },
+    {
+      altStroke: "#661c0a",
+      fill: "#f3a8be",
+    },
+    {
+      altStroke: "#912b11",
+      fill: "#b43718",
+    },
+  ],
+
+  // 2ndddddddddddddddddddddd oneeeeeeeeeeeeeeeeeeeeeeeeeeee
+
+  [
+    {
+      altStroke: "#1a3a38",
+      fill: "#2ab653",
+    },
+    {
+      altStroke: "#109d4c",
+      fill: "#287891",
+    },
+    {
+      altStroke: "#66665d",
+      fill: "#2a9d98",
+    },
+    {
+      altStroke: "#640a60",
+      fill: "#8ab6ad",
+    },
+    {
+      altStroke: "#899aa4",
+      fill: "#baaaa4",
+    },
+    {
+      altStroke: "#ba7856",
+      fill: "#eeee6a",
+    },
+    {
+      altStroke: "#c2bdcc",
+      fill: "#f44461",
+    },
+    {
+      altStroke: "#baa73f",
+      fill: "#676f51",
+    },
+    {
+      altStroke: "#9e700e",
+      fill: "#b212ba",
+    },
+    {
+      altStroke: "#4abc6e",
+      fill: "#5abf94",
+    },
+    {
+      altStroke: "#66111a",
+      fill: "#faaabe",
+    },
+    {
+      altStroke: "#912131",
+      fill: "#bbbb18",
+    },
+  ],
+];
